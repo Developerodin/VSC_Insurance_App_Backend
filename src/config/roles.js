824e3f788a5350a591b23ca8fbef81c8bd8bd1be
Akshaypareek01 <akshay96102@gmail.com>
2025-05-13 14:46:54 +0530

    working

--- src/config/roles.js
+++ src/config/roles.js
@@ -3,4 +3,4 @@ export const roles = ['user', 'admin', 'superAdmin'];
 export const roleRights = new Map();
 roleRights.set(roles[0], ['getUsers']);
 roleRights.set(roles[1], ['getUsers', 'manageUsers']);
-roleRights.set(roles[2], ['getUsers', 'manageUsers', 'manageAdmins','manageProducts','manageCategories','manageSubcategories']);
+roleRights.set(roles[2], ['getUsers', 'manageUsers', 'manageAdmins','manageProducts','manageCategories','manageSubcategories','manageLeads','manageBankAccounts','getCategories','getSubcategories','getLeads','getProducts','getBankAccounts','manageTransactions','getTransactions','manageCommissions','getCommissions','manageNotifications','getNotifications','manageSettings','getSettings']);
