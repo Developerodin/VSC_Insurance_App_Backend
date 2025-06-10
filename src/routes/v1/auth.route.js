c82bf85495f5dbb7889c4ccdf6b58cfe1db0252a
akshaypareek01 <akshay96102@gmail.com>
2025-06-10 14:26:00 +0530

    working

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -8,7 +8,7 @@ import auth from '../../middlewares/auth.js';
 const router = express.Router();
 
 // router.post('/register', validate(authValidation.register), authController.register);
-// router.post('/login', validate(authValidation.login), authController.login);
+router.post('/login', validate(authValidation.login), authController.login);
 // router.post('/logout', validate(authValidation.logout), authController.logout);
 // router.post('/refresh-tokens', validate(authValidation.refreshTokens), authController.refreshTokens);
 // router.post('/forgot-password', validate(authValidation.forgotPassword), authController.forgotPassword);
